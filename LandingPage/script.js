2c9ea9b751d17ef75df30bd72bfe3234166ae66e
manalipatil31 <patillmanali@gmail.com>
2025-07-03 22:52:55 +0530

    Add files via upload

---
+++ LandingPage/script.js
@@ -0,0 +1,5 @@
+// Optional: You can add form interactivity here
+document.querySelector('.subscribe-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thanks for subscribing!');
+});
